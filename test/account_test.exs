2f862adea5e289d5398620938172cb96ef662fc4
Julien Lucca <julienlucca@gmail.com>
2024-03-09 15:11:20 -0300

    Fix account tests to follow the new pattern

--- test/account_test.exs
+++ test/account_test.exs
@@ -4,13 +4,15 @@ defmodule XRPL.AccountTest do
   alias XRPL.Account
   alias XRPL.Account, as: Account
 
-  @account "rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn"
-  @destination_account "r9Co1BjUUGcZSxdQ7Zie8dTkXxhs4QhHjo"
+  @account "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn"
+  @destination_account "ra5nK24KXen9AHvsdFTKHSANinZseWnPcX"
 
   describe "account_channels/1" do
     test "returns information about an account's Payment Channels" do
       params = %{account: @account, destination_account: @destination_account}
-      assert {:ok, %Tesla.Env{status: 200}} = Account.account_channels(params)
+
+      assert {:ok, %{"account" => @account}} =
+               Account.account_channels(params)
     end
 
     test "it returns an error if we don't provide the required param" do
@@ -28,7 +30,8 @@ defmodule XRPL.AccountTest do
 
   describe "account_currencies/1" do
     test "returns information about account currencies" do
-      assert {:ok, %Tesla.Env{status: 200}} = Account.account_currencies(%{account: @account})
+      assert {:ok, _} =
+               Account.account_currencies(%{account: @account})
     end
 
     test "it returns an error if we don't provide the required param" do
@@ -45,7 +48,7 @@ defmodule XRPL.AccountTest do
 
   describe "account_info/1" do
     test "returns information about an account" do
-      assert {:ok, %Tesla.Env{status: 200}} =
+      assert {:ok, %{"account_data" => %{"Account" => @account}}} =
                Account.account_info(%{account: @account})
     end
 
@@ -58,12 +61,16 @@ defmodule XRPL.AccountTest do
       assert_raise XRPL.Error, "XRPL call failed: Invalid params", fn ->
         Account.account_info!(%{})
       end
+
+      assert_raise XRPL.Error, "XRPL call failed: Invalid params", fn ->
+        Account.account_info!(%{account: "not_an_account"})
+      end
     end
   end
 
   describe "account_lines/1" do
     test "returns information about an account's trust lines" do
-      assert {:ok, %Tesla.Env{status: 200}} =
+      assert {:ok, %{"account" => @account}} =
                Account.account_lines(%{account: @account})
     end
 
@@ -81,13 +88,16 @@ defmodule XRPL.AccountTest do
 
   describe "account_nfts/1" do
     test "returns information about an account's nfts" do
-      assert {:ok, %Tesla.Env{status: 200}} =
+      assert {:ok, %{"account" => @account}} =
                Account.account_nfts(%{account: @account})
     end
 
     test "it returns an error if we don't provide the required param" do
       assert {:error, %{errors: errors}} = Account.account_nfts(%{})
       assert errors == [account: {"can't be blank", [validation: :required]}]
+
+      assert {:error, %{errors: errors}} = Account.account_nfts(%{account: "not_an_account"})
+      assert errors == [account: {"has invalid format", [validation: :format]}]
     end
 
     test "calling the ! version of the function raises an error if the request fails" do
@@ -99,7 +109,7 @@ defmodule XRPL.AccountTest do
 
   describe "account_objects/1" do
     test "returns information about an account's objects" do
-      assert {:ok, %Tesla.Env{status: 200}} =
+      assert {:ok, %{"account" => @account}} =
                Account.account_objects(%{account: @account})
     end
 
@@ -117,7 +127,7 @@ defmodule XRPL.AccountTest do
 
   describe "account_offers/1" do
     test "returns information about an account's offers" do
-      assert {:ok, %Tesla.Env{status: 200}} =
+      assert {:ok, %{"account" => @account}} =
                Account.account_offers(%{account: @account})
     end
 
@@ -135,7 +145,7 @@ defmodule XRPL.AccountTest do
 
   describe "account_tx/1" do
     test "returns information about an account's transactions" do
-      assert {:ok, %Tesla.Env{status: 200}} =
+      assert {:ok, %{"account" => @account}} =
                Account.account_tx(%{account: @account})
     end
 
